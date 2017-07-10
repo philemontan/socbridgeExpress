ceeabbc4898f2c3d9cb49576d69b1b1178eba9a3
philemontan <philemontan@gmail.com>
2017-07-10 17:37:34 +0800

    chat

--- app.js
+++ app.js
@@ -11,18 +11,17 @@ var mongoose = require('mongoose');
 var index = require('./routes/index');
 var user = require('./routes/user');
 var wallPosts = require('./routes/wallPosts');
-var chat = require('./routes/chat');
 
 //local MongoDB
-// mongoose.connect('localhost:27017/test');
+mongoose.connect('localhost:27017/test');
 var options = {
-server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+    server: {socketOptions: {keepAlive: 300000, connectTimeoutMS: 30000}},
+    replset: {socketOptions: {keepAlive: 300000, connectTimeoutMS: 30000}}
 };
 
 //mongoLAB, ID:admin, pw: admin
 var mongodbUri = 'mongodb://admin:admin@ds135812.mlab.com:35812/socbridge';
-mongoose.connect(mongodbUri, options);
+// mongoose.connect(mongodbUri, options);
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
@@ -32,10 +31,10 @@ mongoose.connect(mongodbUri, options);
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
 
-app.use(function(req, res, next) { // Allowing CORS
+app.use(function (req, res, next) { // Allowing CORS
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -47,27 +46,83 @@ app.use(express.static(path.join(__dirname, 'dist')));
 //Set REST routes
 app.use('/user', user);
 app.use('/posts', wallPosts);
-app.use('/chat', chat);
 
 //Catch all other routes and return the index file
 app.use('*', index);
 
-//Socket IO
+//Socket IO + variables
 var io = socket_io();
 app.io = io;
 
-//Socket.io starts listening
-io.on('connection', function(socket) {
-   console.log('A user connected');
+var roomArr = {};
 
-   //methods for the particular instance of socket
-   socket.on('new message', function(msg) {
-       console.log('new message: ' + msg);
-       app.io.emit('chat message', msg);
-   })
+//Socket.io starts listening ==> all chat logic is here
+io.on('connection', function (socket) {
+    socket.on('subscribe', function (joiningInfo) {// index 0 = first name. index 1 = room name.
+        // // unsubscribe to everything other room
+        // for (var property in roomArr) {
+        //     if (roomArr.hasOwnProperty(property) && property != joiningInfo[1]) { // if current room is not the room to be joined
+        //         //remove user from current room
+        //         var userIdxInRoom = roomArr[property].indexOf(joiningInfo[0]);
+        //         if (userIdxInRoom != null) { //checks if user is in the room
+        //             roomArr[property].splice(userIdxInRoom, 1);
+        //             io.in(property).emit('room-info', roomArr[property]);
+        //             io.in(property).emit('left-message', joiningInfo[0] + ' has left room: ' + property);
+        //             //unplugging socket
+        //             console.log(joiningInfo[0] + ' is leaving room ' + property);
+        //             socket.leave(property);
+        //         }
+        //     }
+        // }
+        // subscribe to particular room
+        if (joiningInfo[1] in roomArr) { // room for particular module exist
+            if (!roomArr[joiningInfo[1]].includes(joiningInfo[0])) { // user was not in room before
+                roomArr[joiningInfo[1]].push(joiningInfo[0]);
+            }
+            // subscribing to room
+            console.log(joiningInfo[0] + ' has joined room:' + joiningInfo[1]);
+            socket.join(joiningInfo[1]);
+            //emit to all client sockets that a member has joined.
+            io.in(joiningInfo[1]).emit('joined-message', joiningInfo[0] + ' has joined room: ' + joiningInfo[1]);
+            //emitting to specific client the current member list
+            io.in(joiningInfo[1]).emit('room-info', roomArr[joiningInfo[1]]);
 
+        }
+        else { // room for particular module does not exist
+            roomArr[joiningInfo[1]] = [];
+            roomArr[joiningInfo[1]].push(joiningInfo[0]);
+            // subscribing to room
+            console.log(joiningInfo[0] + ' has joined room:' + joiningInfo[1]);
+            socket.join(joiningInfo[1]);
+            //emit to all client sockets that a member has joined.
+            io.in(joiningInfo[1]).emit('joined-message', joiningInfo[0] + ' has joined room: ' + joiningInfo[1]);
+            //emitting to specific client the current member list
+            io.in(joiningInfo[1]).emit('room-info', roomArr[joiningInfo[1]]);
+        }
+    });
+    socket.on('unsubscribe', function (leavingInfo) {//index 0 = first name. index 1 = room name.
+        console.log(leavingInfo[0] + ' is leaving room ' + leavingInfo[1]);
+        socket.leave(leavingInfo[1]);
+    });
+    socket.on('send', function (data) {
+        console.log('sending message');
+        io.in(data.room).emit('message', data.message);
+    });
+    socket.on('leave-all', function (userFirstName) {
+        for (var property in roomArr) {
+            //removing from property arr
+            var userIdxInRoom = roomArr[property].indexOf(userFirstName);
+            if (userIdxInRoom != null) {
+                roomArr[property].splice(userIdxInRoom, 1);
+                io.in(property).emit('room-info', roomArr[property]);
+                io.in(property).emit('left-message', userFirstName + ' has left room: ' + property);
+                //unplugging socket
+                console.log(userFirstName + ' is leaving room ' + property);
+                socket.leave(property);
+            }
+        }
+    });
 });
-
 
 
 //Catch all other routes and return the index file
